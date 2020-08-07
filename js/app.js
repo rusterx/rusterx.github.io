0bb99a5fee15991b59b9d557b0d59a3a51f448ab
xing <1281961491@qq.com>
2020-08-07 16:25:16 +0800

    revise selector

--- js/app.js
+++ js/app.js
@@ -2,19 +2,17 @@ $(window).load(function(){
   $(document).keydown(function(e) {
     // left
     if(e.keyCode == 37) {
-      var preList = $('previous');
+      var preList = $('.previous');
       if(preList.length != 0){
-        var path = $(preList[0]).attr('href');
-        window.location = 'https://xingtingyang.com' + path;
+        preList[0].click();
       }
     }
     // right
     if(e.keyCode == 39) {
-      var nextList = $('next');
+      var nextList = $('.next');
       console.log(nextList.length);
       if(nextList.length != 0){
-        var path = $(nextList[0]).attr('href');
-        window.location = 'https://xingtingyang.com' + path;
+        nextList[0].click();
       }
     }
   });
