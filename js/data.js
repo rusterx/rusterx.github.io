ca9e3f7f1f50443c1be207463a25ecaa4b25c9e6
xing <1281961491@qq.com>
2024-06-21 17:13:23 +0800

    add data.js

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ posts = [
       "id": {{ counter }},
       "url": "{{ site.url }}{{ page.url }}",
       "title": "{{ page.title }}",
-      "body": "{{ page.date | date: '%Y/%m/%d' }} - {{ page.content | markdownify | strip_html | replace: '\', '\\' }}"
+      "body": "{{ page.date | date: '%Y/%m/%d' }} - {{ page.content | markdownify | strip_html | strip_newlines | replace: '\', '\\'}}"
       {%- assign counter = counter | plus: 1 -%}
     }
     {%- if forloop.last -%}
