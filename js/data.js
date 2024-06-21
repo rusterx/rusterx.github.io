fb71edc8fec19148a028b23d7e98e47f96d0eba1
xing <1281961491@qq.com>
2024-06-21 19:42:44 +0800

    add data.js

--- js/data.js
+++ js/data.js
@@ -1,16 +1,14 @@
-posts = [
+var posts = [
     {%- for page in site.posts -%}
-    {
-      "id": {{ counter }},
-      "url": "{{ site.url }}{{ page.url }}",
-      "title": "{{ page.title }}",
-      "body": "{{ page.date | date: '%Y/%m/%d' }} - {{ page.content | markdownify | strip_html | strip_newlines | replace: '\', '\\'}}"
-      {%- assign counter = counter | plus: 1 -%}
-    }
-    {%- if forloop.last -%}
-    {%- else -%}, 
-    {%- endif -%}
-    {%- endfor -%}
-]
+        {
+          "id": {{ counter }},
+          "url": "{{ site.url }}{{ page.url }}",
+          "title": "{{ page.title }}",
+          "body": "{{ page.date | date: "%Y/%m/%d" }} - {{ page.content | markdownify | replace: '.', '. ' | replace: '\', '' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"{% assign counter = counter | plus: 1 %}
+        }
+        {%- if forloop.last -%}
+        {%- else -%}, 
+        {%- endif -%}
+    {%- endfor -%}];
 
 export {posts}
